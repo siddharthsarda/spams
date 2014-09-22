d7262f902784768e6ad20b10c2d173e84d8d0497
Siddharth <ssarda@student.ethz.ch>
2014-09-22 15:42:56 +0200

    Renaming file and some other minor changes

---
+++ spams/db/utils.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import Table, MetaData
+
+from spams.config import CONNECTION_STRING
+
+
+def create_postgres_engine():
+    return create_engine(CONNECTION_STRING)
+
+
+def get_table(tablename, metadata):
+    return Table(tablename, metadata, autoload=True)
+
+
+def get_metadata(engine):
+    return MetaData(engine)
+
+
+def get_connection(engine):
+    return engine.connect()
+
+
+def create_and_return_session(engine=None):
+    if not engine:
+        engine = create_postgres_engine()
+    Session = sessionmaker(bind=engine)
+    return Session()
--- spams/tests/test_db.py
+++ spams/tests/test_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import MetaData
 
-from spams.db.connect import create_postgres_engine
+from spams.db.utils import create_postgres_engine
 
 if __name__ == "__main__":
     engine = create_postgres_engine()
