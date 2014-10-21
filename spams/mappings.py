16ddbf78bdc0a7f01a5d725b0c754f00e16aebf2
Siddharth <ssarda@student.ethz.ch>
2014-10-21 12:08:04 +0200

    Add new mappings file

---
+++ spams/mappings.py
@@ -0,0 +1,16 @@
+LABEL_PLACE_MAPPING = {
+    2: "Home of a Friend",
+    6: "Indoor Sports",
+    3: "Work",
+    8: "Bar;Restaurant",
+    7: "Outdoor Sports",
+    10: "Holidays Resort",
+    1: "Home",
+    4: "Transport Related",
+    5: "Work of a Friend",
+    9: "Shop"
+}
+AGE_MAPPING = {1: "<16", 2: "16-21", 3: "22-27", 4: "28-33", 5: "33-38", 6: "39-44", 7: "45-50", 8: ">50"}
+GENDER_MAPPING = {1: "male", 2: "female"}
+WORKING_MAPPING = {1: "Working Full time", 2: "Working Part time", 3: "Not Currently Working", 4: "Studying full time", 5: "Studying Part time", 6: "Housewife", 7: "Retired", 8: "Other"}
+BILL_MAPPING = {1: "myself", 2: "parents", 3: "spouse", 4: "employer", 5: "other"}
