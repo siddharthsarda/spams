9202529fcc779212c1fd86d0ee44887870e4caa8
Siddharth <ssarda@student.ethz.ch>
2014-10-31 14:47:54 +0100

    Adding comments

--- spams/density_inference/perform_kde.py
+++ spams/density_inference/perform_kde.py
@@ -18,19 +18,19 @@ if __name__ == "__main__":
     estimators = {}
     test_set = {}
     training_set = []
-    for label in xrange(1, 11):
-        lat_long_query = select([places_location.c.latitude, places_location.c.longitude]).where(places_location.c.place_label_int==label)
+    for label_id in xrange(1, 11):
+        label = LABEL_PLACE_MAPPING[label_id]
+        lat_long_query = select([places_location.c.latitude, places_location.c.longitude]).where(places_location.c.place_label_int==label_id)
         results = connection.execute(lat_long_query).fetchall()
         result_len = len(results)
+        # Use 1 % of the data as test set
         test_len = math.ceil(result_len * 0.1)
-        latitudes = [float(r[0]) for r in results]
-        longitudes = [float(r[1]) for r in results]
-        xy = zip(latitudes, longitudes)
+        xy = [(float(r[0]), float(r[1])) for r in results]
         test_indices = np.random.choice(result_len, test_len)
         training_set =[]
-        label = LABEL_PLACE_MAPPING[label]
         test_set[label] = []
         training_set = []
+        # Separate test and training values
         for index, val in enumerate(xy):
             if index in test_indices:
                 test_set[label].append(val)
@@ -38,26 +38,30 @@ if __name__ == "__main__":
                 training_set.append(val)
 
         xy = np.array(training_set)
+        # Convert to radians
         xy *= np.pi /180.
         params = {'bandwidth': np.logspace(-3, 3, 20)}
-        grid = GridSearchCV(KernelDensity(), params)
+        # do a grid search
+        grid = GridSearchCV(KernelDensity(metric="haversine", algorithm="ball_tree"), params)
         grid.fit(xy)
         estimators[label] = grid.best_estimator_
-        print grid.best_estimator_.bandwidth
     accurate = 0.0
     counter = 0.0
+    # Test accuracy using just kde
     for label in test_set:
         for value in test_set[label]:
+            # Convert to radians
             value = [v * np.pi / 180. for v in value]
             max_prob_density =  -sys.maxint - 1
             best_label = ""
             counter += 1
+            ## Loop over all estimators to find one with the highest density
             for key in estimators:
                 score = estimators[key].score(value)
                 if score > max_prob_density:
                     max_prob_density = score
                     best_label = key
-            print best_label, label
+            #print max_prob_density
             if best_label == label:
                 accurate +=1
     print accurate/counter * 100            
