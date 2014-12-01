5586dc4257b4303cb2811f90184dd7a00b49371d
Siddharth <ssarda@student.ethz.ch>
2014-12-01 14:47:23 +0100

    Almost complete changes to the pipeline

--- spams/density_inference/perform_kde.py
+++ spams/density_inference/perform_kde.py
@@ -11,7 +11,7 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 from spams.mappings import LABEL_PLACE_MAPPING
 from sklearn.grid_search import GridSearchCV
-from itertools import groupby, izip
+from itertools import groupby, izip, product
 
 
 metadata, connection = setup_database()
@@ -25,12 +25,9 @@ def test_kde(test_set, estimators):
     for label in test_set:
         missed[label] = defaultdict(int)
         
-        # print label
-        # print len(test_set[label])
         for value in test_set[label]:
             # Convert to radians
             value = [v * np.pi / 180. for v in value]
-            # print value
             best_label = ""
             counter += 1
             ## Loop over all estimators to find one with the highest density
@@ -38,7 +35,6 @@ def test_kde(test_set, estimators):
             for key in estimators:
                 score_dict[key] = estimators[key].score(value)
             sorted_dict = sorted(score_dict.items(), key=lambda x: x[1], reverse=True)    
-            # print sorted_dict
             sorted_labels = [s[0] for s in sorted_dict]
             if sorted_labels[0] == label:
                 accurate +=1
@@ -46,8 +42,8 @@ def test_kde(test_set, estimators):
                 missed[label][sorted_labels[0]] += 1
 
             nrr += (1.0/(1 + sorted_labels.index(label)))    
-    accuracy = accurate/counter * 100
-    # print missed
+    if counter:
+        accuracy = accurate/counter * 100
     return accurate, nrr, counter
 
     
@@ -55,25 +51,34 @@ def train_kde(training_set):
     xy = np.array(training_set)
     # Convert to radians
     xy *= np.pi /180.
-    params = {'bandwidth': np.logspace(-3, 3, 20)}
+    params = {'bandwidth': np.logspace(-5, 5, 20), 'kernel' : ['gaussian','exponential']}
     # do a grid search
-    grid = GridSearchCV(KernelDensity(metric="haversine", algorithm="ball_tree"), params)
-    grid.fit(xy)
-    # print grid.best_estimator_
-    return grid.best_estimator_
+    try:
+        grid = GridSearchCV(KernelDensity(metric="haversine", algorithm="ball_tree"), params)
+        grid.fit(xy)
+        return grid.best_estimator_
+    except ValueError:
+        k = KernelDensity(metric="haversine", algorithm="ball_tree", bandwidth=0.001, kernel="exponential")
+        k.fit(xy)
+        return k
+
 
 def extract_places(places_location, label_id, restrict_to):
+    if len(restrict_to) == 0:
+        return []
+
     lat_long_query = select([places_location.c.latitude, places_location.c.longitude]).where(places_location.c.place_label_int==label_id).where(places_location.c.id.in_(restrict_to))
     results = connection.execute(lat_long_query).fetchall()
     return [(float(r[0]), float(r[1])) for r in results]
 
 
 def extract_visits(places_location, label_id, restrict_to):
+    if len(restrict_to) == 0:
+        return []
     visits_10min = get_table("visits_10min", metadata)
     visits_with_places = places_location.join(visits_10min, onclause=and_(places_location.c.userid == visits_10min.c.userid, places_location.c.placeid == visits_10min.c.placeid)).alias("visits_with_places")
     query = select([visits_with_places.c.places_location_latitude, visits_with_places.c.places_location_longitude]).where(visits_with_places.c.places_location_place_label_int == label_id).where(visits_with_places.c.places_location_id.in_(restrict_to))
     results = connection.execute(query).fetchall()
-    print len(results)
     return [(float(r[0]), float(r[1])) for r in results]
 
 def split_test_and_train(places_location):
@@ -95,29 +100,30 @@ def perform_kde(places_location, test, train, input_func= extract_places):
     test_set_dict = {}
     training_set = []
     for label_id in xrange(1, 11):
+        if len(train[label_id]) == 0 or len(test[label_id]) == 0:
+            continue
         label = LABEL_PLACE_MAPPING[label_id]
         training_set = input_func(places_location, label_id, train[label_id])
         estimators[label] = train_kde(training_set)
         test_set_dict[label] = [(float(r[0]), float(r[1])) for r in connection.execute(select([places_location.c.latitude, places_location.c.longitude]).where(places_location.c.id.in_(test[label_id]))).fetchall()]
     accuracy, mrr, test_set_size = test_kde(test_set_dict, estimators)
-    #print accuracy
     return accuracy, mrr, test_set_size
 
 
-def perform_kde_visits(places_location, restrict_to=None):
-    return perform_kde(places_location, input_func=extract_visits, restrict_to=restrict_to)
+def perform_kde_visits(places_location, test, train, restrict_to=None):
+    return perform_kde(places_location, test, train, input_func=extract_visits)
 
 
-def perform_kde_places(places_location, restrict_to=None):
-    return perform_kde(places_location, restrict_to=restrict_to)
+def perform_kde_places(places_location, test, train):
+    return perform_kde(places_location, test, train, input_func=extract_places)
 
 
-def perform_db_scan(places_location):
+def perform_db_scan(places_location, eps=0.02, min_samples=30):
     lat_long_query = select([places_location.c.latitude, places_location.c.longitude, places_location.c.id])
     results = connection.execute(lat_long_query).fetchall()
     xy = np.asarray([(float(r[0]), float(r[1])) for r in results])
     places = [r[2] for r in results]
-    db = DBSCAN(eps =0.025, metric="haversine", algorithm="ball_tree", min_samples=20).fit(xy)
+    db = DBSCAN(eps, metric="haversine", algorithm="ball_tree", min_samples = min_samples).fit(xy)
     groups = db.labels_
     places_with_groups = izip(places, groups)
     place_group_dict = defaultdict(int)
@@ -127,33 +133,39 @@ def perform_db_scan(places_location):
         group_place_dict[group].append(place)
     return place_group_dict, group_place_dict
 
-def kde_with_db_scan(places_location):
-    pass
+def kde_with_db_scan(places_location, test, train, input_func=extract_places):
+    place_group_dict, group_place_dict = perform_db_scan(places_location)
+    groups = group_place_dict.keys()
+    group_train_dict = {}
+    group_test_dict = {}
+    for g in groups:
+        group_train_dict[g] = {}
+        group_test_dict[g] = {}
+        for label in train.keys():
+            group_train_dict[g][label] = [p for p in train[label] if place_group_dict[p] == g]
+            group_test_dict[g][label] = [p for p in test[label] if place_group_dict[p] == g]
+        
+    net_acc = 0.0
+    net_nrr = 0.0
+    net_counter = 0.0
+    for g in groups:
+        accurate, nrr, counter = perform_kde(places_location, group_test_dict[g], group_train_dict[g], input_func=input_func)
+        net_acc += accurate
+        net_nrr += nrr
+        net_counter += counter
+    return net_acc, net_nrr, net_counter
+
 
 if __name__ == "__main__":
     places_location = get_table("places_location", metadata)
-    #min_lat, max_lat, min_long, max_long = connection.execute(select([func.min(places_location.c.latitude), func.max(places_location.c.latitude), func.min(places_location.c.longitude), func.max(places_location.c.longitude)])).fetchall()[0]
-    #print perform_kde_visits(places_location)
-    #perform_db_scan(places_location)
-    # place_group_dict, group_place_dict = perform_db_scan(places_location)
-    overall_a = 0.0
-    
-    # accurate, nrr, counter = perform_kde(places_location,test, train, input_func=extract_visits)
-    # print accurate, nrr, counter
+
+    acc = 0.0
+    nrr = 0.0
     for i in xrange(10):
         test, train = split_test_and_train(places_location)
-        accurate, nrr, counter = perform_kde(places_location,test, train, input_func=extract_visits)
-        overall_a += (accurate/counter)
-    print overall_a/10
-    
-    #for db_scan in (True, False):
-    #    av = 0.0
-    #    mrr = 0.0
-    #    for i in xrange(1, 100):
-    #        a, m, _ = perform_kde_places(places_location, with_db_scan=db_scan)
-    #        av += a
-    #        mrr += m
-    #    print (av/100, mrr/100)
-    
-    #print perform_kde_visits(places_location, with_db_scan=True)
-    #print perform_kde_visits(places_location, with_db_scan=False)
+        #a, n, counter = perform_kde_visits(places_location, test, train)
+        a, n, counter = kde_with_db_scan(places_location, test, train, input_func=extract_visits)
+        #a,n = kde_with_db_scan(places_location, test, train, input_func=extract_visits)
+        acc += a/counter
+        nrr += n/counter
+    print acc/10, nrr/10
