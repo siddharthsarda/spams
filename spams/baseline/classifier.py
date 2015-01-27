f9198ae66ba172a43045b8ac8cecd67766131b67
Siddharth <ssarda@student.ethz.ch>
2015-01-27 11:40:30 +0100

    Classifier now records accuracy of individual labels

--- spams/baseline/classifier.py
+++ spams/baseline/classifier.py
@@ -63,19 +63,25 @@ def classify_top_level(x_train, y_train, x_test, y_priors=None):
     logging.debug(clf)
     return clf.predict(x_test)
 
-def train_classifier_and_predict(training, test, y_priors=None):
+def train_classifier_and_predict(training, test, y_priors=None, class_weight= None):
     if len(test) == 0:
         return 0, len(test)
     y_train, x_train = zip(*training) 
     y_test, x_test = zip(*test)
+    places = [y[0] for y in y_test]
+    y_test = [y[1] for y in y_test]
+    y_train = [y[1] for y in y_train]
     sample_weight = None
-    pipeline = Pipeline([('selection', SelectFpr(chi2, alpha=0.05)),('scaler', StandardScaler()),('svm', svm.SVC())])
+    pipeline = Pipeline([('selection', SelectFpr(chi2, alpha=0.05)),('scaler', StandardScaler()),('svm', svm.SVC(class_weight = class_weight))])
     clf = GridSearchCV(pipeline, params)
     clf.fit(x_train, y_train)
     clf = clf.best_estimator_
     logging.debug(clf)
-    result = [y == y_test[index] for index, y in enumerate(clf.predict(x_test))]
-    return result.count(1), len(result)
+    predictions = clf.predict(x_test)
+    answers = zip(places, predictions, y_test)
+    result = [y == y_test[index] for index, y in enumerate(predictions)]
+    
+    return result.count(1), len(result), answers
 
 
 def classify_other(training, test, y_priors=None):
@@ -84,9 +90,9 @@ def classify_other(training, test, y_priors=None):
     y_train, x_train = zip(*training)
     y_test, x_test = zip(*test) 
     sample_weight = None
-    y_training_other = [OTHER_MAPPING[y] for y in y_train]
-    sports_training = [(y, x) for (y, x) in training if y in [6, 7]]
-    shop_and_food_training = [(y, x) for (y, x) in training if y in [8, 9]]
+    y_training_other = [OTHER_MAPPING[y[1]] for y in y_train]
+    sports_training = [(y, x) for (y, x) in training if y[1] in [6, 7]]
+    shop_and_food_training = [(y, x) for (y, x) in training if y[1] in [8, 9]]
     pipeline = Pipeline([('selection', SelectFpr(chi2, alpha=0.05)),('scaler', StandardScaler()),('svm', svm.SVC())])
     clf = GridSearchCV(pipeline, params)
     clf.fit(x_train, y_training_other)
@@ -97,6 +103,7 @@ def classify_other(training, test, y_priors=None):
     count = 0.0
     sports_test = []
     food_shop_test = []
+    answers = []
     for index, val in enumerate(result):
         if val == 0:
            sports_test.append(test[index])
@@ -104,19 +111,22 @@ def classify_other(training, test, y_priors=None):
            food_shop_test.append(test[index])
         elif val == 2 or val == 3: 
            count += 1
-           accurate += REVERSE_OUTER_MAPPING[val] == y_test[index]
-    a,c  = train_classifier_and_predict (shop_and_food_training, food_shop_test)
+           accurate += REVERSE_OUTER_MAPPING[val] == y_test[index][1]
+           answers.append((y_test[index][0], REVERSE_OUTER_MAPPING[val], y_test[index][1]))
+    a,c,d  = train_classifier_and_predict (shop_and_food_training, food_shop_test)
     accurate += a
     count += c
-    a,c  = train_classifier_and_predict(sports_training, sports_test)
+    answers.extend(d)
+    a,c,d  = train_classifier_and_predict(sports_training, sports_test)
     accurate += a
     count += c
-    return accurate, count 
+    answers.extend(d)
+    return accurate, count, answers 
 
 def top_level_accuracy(top_level_predictions, test_set):
     accurate = 0.0 
     for index, pred in enumerate(top_level_predictions):
-         if pred == TOP_LEVEL_MAPPING[test_set[index][0]]:
+         if pred == TOP_LEVEL_MAPPING[test_set[index][0][1]]:
              accurate += 1
     return accurate/len(top_level_predictions)         
              
@@ -131,27 +141,30 @@ def perform_multi_level_classification(places_features):
     for place, user in places_features:
         label, features = places_features[(place, user)]
         X.append(features)
-        Y.append(label)
+        Y.append(((place, user) , label))
         Z.append((place, user, label))
     X = np.array(X)
     Y = np.array(Y)
     Z = np.array(Z)
     n = Y.shape[0]
     kf = KFold(n=n, n_folds=KFOLDS)
-    overall_accuracy = 0.0
     tla = 0.0
-    home_accuracy = 0.0
+    overall_accuracy = 0.0
+    accuracy_other = 0.0
+    each_label_accuracy = {}
+    for label in xrange(1, 11):
+        each_label_accuracy[label] = [0, 0]
     for train_index, test_index in kf:
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = Y[train_index], Y[test_index]
         z_train, z_test = Z[train_index], Z[test_index]
         priors = priors_with_db_scan(z_test, z_train) 
         training_dataset = zip(y_train, X_train)
-        home_training_dataset = [(y, x) for (y, x) in training_dataset if y in [1, 2]]
-        work_training_dataset = [(y, x) for (y, x) in training_dataset if y in [3, 5]]
-        other_training_dataset = [(y, x) for (y, x) in training_dataset if y in [4, 6, 7, 8, 9, 10]]
+        home_training_dataset = [(y, x) for (y, x) in training_dataset if y[1] in [1, 2]]
+        work_training_dataset = [(y, x) for (y, x) in training_dataset if y[1] in [3, 5]]
+        other_training_dataset = [(y, x) for (y, x) in training_dataset if y[1] in [4, 6, 7, 8, 9, 10]]
         test_set = zip(y_test, X_test)
-        y_train_top_level = [TOP_LEVEL_MAPPING[y] for y in y_train]
+        y_train_top_level = [TOP_LEVEL_MAPPING[y[1]] for y in y_train]
         priors_top_level = [TOP_LEVEL_MAPPING[y] for y in priors]
         
         #priors_top_level = [random.randint(1,3) for y in priors]
@@ -169,14 +182,35 @@ def perform_multi_level_classification(places_features):
             else:
                 other_input.append(test_set[index])
         logging.debug((len(home_input), len(work_input), len(other_input)))        
-        h_n, h_d = train_classifier_and_predict(home_training_dataset, home_input)
-        home_accuracy += (h_n*1.0)/(h_d *1.0)
-        w_n, w_d = train_classifier_and_predict(work_training_dataset, work_input)
-        o_n, o_d = classify_other(other_training_dataset, other_input)
+        h_n, h_d, home_answers = train_classifier_and_predict(home_training_dataset, home_input)
+        w_n, w_d, work_answers = train_classifier_and_predict(work_training_dataset, work_input)
+        o_n, o_d, other_answers = classify_other(other_training_dataset, other_input)
         overall_accuracy += ((h_n + w_n + o_n) * 1.0 )/ ((h_d + w_d + o_d) * 1.0)
-    print tla/len(kf)
-    print home_accuracy/len(kf)
-    return overall_accuracy/ len(kf) 
+        answers = []
+        answers.extend(home_answers)
+        answers.extend(work_answers)
+        answers.extend(other_answers)
+        a_d = {}
+        a_other =0.0
+        for place, pred, label in answers:
+            a_d[place] = (pred, label)
+            each_label_accuracy[label][1] += 1
+            if pred == label:
+                a_other += 1
+                each_label_accuracy[label][0] += 1
+        accuracy_other += a_other/len(answers)
+    a = 0.0
+    c = 0.0
+    for p, l in each_label_accuracy.values():
+        a += p
+        c += l
+    #for label in xrange(1, 11):
+    #    print (each_label_accuracy[label][0] * 1.0) / each_label_accuracy[label][1]
+
+    print tla/ len(kf)
+    #print accuracy_other/ len(kf)
+    return overall_accuracy/ len(kf)
+
 
 if __name__ == "__main__":
     places_features = {}
